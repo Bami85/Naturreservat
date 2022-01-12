e324464a68e0898124ead5c6147793083d83404b
A.BAMI <91563447+Bami85@users.noreply.github.com>
2022-01-13 00:03:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Naturresevat</title>
+</head>
+<body>
+<form action="./results.php" method="POST">
+ Apa <input placeholder="Apa" type="text" name="Apa">
+ Giraffa <input placeholder="Giraff" type="text" name="Giraff">
+ Coconut <input placeholder="Kokosnöt" type="text" name="Coconut">
+ Tiger <input placeholder="Tiger" type="text" name="Tiger">
+  <button type="submit">spara</button>
+</form>
+</body>
+</html>
