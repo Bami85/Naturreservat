2d1f37b0fc0c42a636f2335764bbfce9ba92b646
A.BAMI <91563447+Bami85@users.noreply.github.com>
2022-01-14 00:14:11 +0100

    Add files via upload

---
+++ visaResults.php
@@ -0,0 +1,33 @@
+<?php
+    include "./class.php";
+
+    $apa = $_POST["apa"];
+    $tiger = $_POST["tiger"];
+    $giraffe = $_POST["giraffe"];
+    $coconut = $_POST["coconut"];
+
+
+    for ($i = 0; $i < $apa; $i++) {
+        $animal1 = new Apor("Apa");
+        echo $animal1->getImage() . "<br>" . $animal1->makeSound();
+    }
+    for ($i = 0; $i < $tiger; $i++) {
+        $animal1 = new Tiger("Tiger");
+        echo $animal1->getImage() . "<br>" . $animal1->makeSound();
+    }
+    for ($i = 0; $i < $giraffe; $i++) {
+        $animal1 = new Giraffe("Giraffe");
+        echo $animal1->getImage() . "<br>" . $animal1->makeSound();
+    }
+    for ($i = 0; $i < $coconut; $i++) {
+        $animal1 = new Coconut("Coconut");
+        echo $animal1->getImage() . "<br>" . $animal1->makeSound();
+    }
+
+   
+       
+    
+?>
+
+
+    
