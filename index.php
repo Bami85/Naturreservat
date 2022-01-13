2b92ee631f29906565354c7540ea037de598c181
A.BAMI <91563447+Bami85@users.noreply.github.com>
2022-01-13 10:05:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Naturresevat</title>
-</head>
+  </head>
 <body>
 <form action="./results.php" method="POST">
  Apa <input placeholder="Apa" type="text" name="Apa">
