2b8ebf33f36fca13cfa5c5f3b51dec9fe7e1e9d2
A.BAMI <91563447+Bami85@users.noreply.github.com>
2022-01-13 11:33:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Naturresevat</title>
-  </head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Naturreservat</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
-<form action="./results.php" method="POST">
- Apa <input placeholder="Apa" type="text" name="Apa">
- Giraffa <input placeholder="Giraff" type="text" name="Giraff">
- Coconut <input placeholder="Kokosnöt" type="text" name="Coconut">
- Tiger <input placeholder="Tiger" type="text" name="Tiger">
-  <button type="submit">spara</button>
-</form>
+    <div class="form-container">
+        <h1>Naturreservat</h1>
+        <form action="./results.php" method="POST">
+            <label>Antal apor</label>
+            <input type="number" name="apa" min="0">
+
+            <label>Antal giraffer</label>
+            <input type="number" name="giraffe" min="0">
+
+            <label>Antal tigrar</label>
+            <input type="number" name="tiger" min="0">
+
+            <label>Antal kokosnötter</label>
+            <input type="number" name="coconut" min="0">
+            
+            <button type="submit" name="submit">Skicka</button>
+        </form>
+    </div>
+
+   
+
 </body>
 </html>
